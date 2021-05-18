c34836b03369bdb31b50af43e70b868ad6b760ce
Blucious <27786206@qq.com>
2021-05-18 21:52:43 +0800

    小修改

---
+++ app/src/main/java/org/group9/stumgr/databinding/DataBindingAdapters.java
@@ -0,0 +1,71 @@
+package org.group9.stumgr.databinding;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.InverseBindingAdapter;
+
+
+public class DataBindingAdapters {
+
+   @SuppressLint("SetTextI18n")
+   @BindingAdapter("android:text")
+   public static void setInteger(EditText view, Integer val) {
+      if (val == null) {
+         view.setText("");
+      } else {
+         view.setText(Integer.toString(val));
+      }
+   }
+
+   @InverseBindingAdapter(attribute = "android:text")
+   public static Integer getInteger(EditText view) {
+      Editable editable = view.getText();
+      if (editable.length() == 0) {
+         return null;
+      }
+      return Integer.parseInt(editable.toString());
+   }
+
+
+   /*
+    ---------------------------------------------------
+    References:
+      Set drawable resource ID in android:src for ImageView using data binding in Android
+      https://stackoverflow.com/questions/35809290/set-drawable-resource-id-in-androidsrc-for-imageview-using-data-binding-in-andr/36941125#36941125
+    */
+   @BindingAdapter("android:src")
+   public static void setImageUri(ImageView view, String imageUri) {
+      if (imageUri == null) {
+         view.setImageURI(null);
+      } else {
+         view.setImageURI(Uri.parse(imageUri));
+      }
+   }
+
+   @BindingAdapter("android:src")
+   public static void setImageUri(ImageView view, Uri imageUri) {
+      view.setImageURI(imageUri);
+   }
+
+   @BindingAdapter("android:src")
+   public static void setImageDrawable(ImageView view, Drawable drawable) {
+      view.setImageDrawable(drawable);
+   }
+
+   @BindingAdapter("android:src")
+   public static void setImageDrawable(ImageView view, Bitmap bitmap) {
+      view.setImageBitmap(bitmap);
+   }
+
+   @BindingAdapter("android:src")
+   public static void setImageResource(ImageView imageView, int resource) {
+      imageView.setImageResource(resource);
+   }
+}
--- app/src/main/java/org/group9/stumgr/ui/UpdateInfoActivity.java
+++ app/src/main/java/org/group9/stumgr/ui/UpdateInfoActivity.java
@@ -16,42 +16,50 @@ import org.group9.stumgr.bean.Student;
 import org.group9.stumgr.service.StudentService;
 
 public class UpdateInfoActivity extends AppCompatActivity {
-    public Student student;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_info);
-        initview();
+   public Student student;
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_update_info);
+      initview();
 
 
+   }
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_student_updateinfo,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      MenuInflater inflater = getMenuInflater();
+      inflater.inflate(R.menu.activity_student_updateinfo, menu);
+      return super.onCreateOptionsMenu(menu);
+   }
 
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId()==R.id.updatedone){
-            StudentService.update(student);
-            Toast.makeText(UpdateInfoActivity.this,"修改成功",Toast.LENGTH_LONG).show();
+   @Override
+   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+      if (item.getItemId() == R.id.updatedone) {
+         int n = StudentService.update(student);
+         Log.d("TAG", "onOptionsItemSelected: n = " + n);
+         Toast.makeText(UpdateInfoActivity.this, "修改成功", Toast.LENGTH_LONG).show();
 
-        }
-        return  super.onOptionsItemSelected(item);
-    }
-    public void initview(){
-        Intent intent = getIntent();
-        int stuId = intent.getIntExtra(UIConstants.BK_STUDENT_ID, -1);
-        this.student = StudentService.getById(stuId);
-        UpdateProfile updateProfile = (UpdateProfile) getSupportFragmentManager().findFragmentById(R.id.updateProfileFr);
-        updateProfile.setStudent(student);
+         //Intent intent = new Intent()
+         //   .putExtra();
+         //setResult(UIConstants.REQ_CODE_DEFAULT);
+         //finish();
+      } else {
+         return super.onOptionsItemSelected(item);
+      }
+      return true;
+   }
+
+   public void initview() {
+      Intent intent = getIntent();
+
+      int stuId = intent.getIntExtra(UIConstants.BK_STUDENT_ID, -1);
+      this.student = StudentService.getById(stuId);
+      UpdateProfile updateProfile = (UpdateProfile) getSupportFragmentManager().findFragmentById(R.id.updateProfileFr);
+      updateProfile.setStudent(student);
 
 
-    }
+   }
 }
